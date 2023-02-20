acb75f25dbf1743582d837e089f446c80b08c75c
Riad_Nageh <riadnageh55@yahoo.com>
2023-02-20 18:35:53 +0200

    learning labs

---
+++ lab-1.py
@@ -0,0 +1,29 @@
+'''
+besm el ab w elebn w alro7 elqudos elah wa7ed amen
+'''
+i=0
+j=0
+line=""
+
+while i<5:
+    while j<i+1:
+        j+=1
+        line+='*'
+    print(line)
+    i+=1
+    j=0
+    line=""
+j=0
+while i>1:
+    while j<i-1:
+        j+=1
+        line+='*'
+    print(line)
+    i-=1
+    j=0
+    line=""
+
+
+
+
+
